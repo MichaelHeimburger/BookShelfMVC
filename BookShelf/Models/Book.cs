3720282da35989510607ba1c65314dd2c27240a8
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-10 15:19:20 -0400

    first commit

---
+++ BookShelf/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookShelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
